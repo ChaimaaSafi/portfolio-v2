c0a35721ad9cbe8f7ef653c9ae200ccbba4635ca
chaimaaSafi <safichaimaa098@gmail.com>
2023-06-09 16:24:03 +0100

    feat: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          hero: "#1c1c1c",
+          primary: "#1f2233",
+          secondary: "#292d40",
+        },
+        yellow: {
+          hero: "#FFE569",
+          primary: "#F8C43A",
+        },
+      },
+    },
   },
   plugins: [],
 };
