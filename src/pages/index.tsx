e42a077ed48e4e453122b437029ae7e28bf66075
chaimaaSafi <safichaimaa098@gmail.com>
2023-06-09 16:25:31 +0100

    feat: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
-export default function IndexPage() {
-  return <h1 className="text-3xl font-bold text-red-400">Hello world!</h1>;
+import Home from "@/components/Home";
+
+export default function Index() {
+  return <Home />;
 }
