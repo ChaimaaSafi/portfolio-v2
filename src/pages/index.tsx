e12be7b7e5c3c743997d9c6a9fe47b63b73e87d9
chaimaaSafi <safichaimaa098@gmail.com>
2023-07-02 22:34:17 +0100

    feat: add About page to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
+import About from "@/components/About";
 import Home from "@/components/Home";
+import { useEffect, useState } from "react";
 
 export default function Index() {
-  return <Home />;
+  const [pageIndex, setpageIndex] = useState<number>(1);
+  useEffect(() => {
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const scrollPositionToTriggerAnimation = 0; // Adjust this value as needed
+    console.log(scrollTop);
+
+    if (scrollTop >= scrollPositionToTriggerAnimation) {
+      setpageIndex(2);
+    } else {
+      setpageIndex(3);
+    }
+  });
+
+  return (
+    <section>
+      <Home />
+      <About />
+    </section>
+  );
 }
