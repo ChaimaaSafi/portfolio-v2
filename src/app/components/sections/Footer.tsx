059281c9fc2c9ace45bcf6d548ae709beebc918a
chaimaaSafi <safichaimaa098@gmail.com>
2023-10-20 23:56:54 +0100

    fix: make footer responsive

--- src/app/components/sections/Footer.tsx
+++ src/app/components/sections/Footer.tsx
@@ -3,13 +3,20 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="py-20 flex justify-center flex-row font-semibold items-center text-base text-white">
-        <div className="w-fit flex flex-row space-x-2">
+    <footer className="py-10 md:py-20 flex justify-center flex-row font-semibold items-center text-sm md:text-base text-white">
+        <div className="hidden md:flex w-fit flex-row space-x-2">
             <p>Crafted with passion and Love</p>
             <span>
               <HeartIcon />
             </span>
             <p> by Chaimaa Safi</p>
+            <p>© [2023]</p>
+        </div>
+        <div className="w-full flex justify-between items-center md:hidden">
+            <p> by Chaimaa Safi</p>
+            <span>
+              <HeartIcon />
+            </span>
             <p>© [2023]</p>
         </div>
     </footer>
