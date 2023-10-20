1eeff4d8d8804198a93842b46a44887b3cb6491f
chaimaaSafi <safichaimaa098@gmail.com>
2023-10-21 00:19:11 +0100

    fix: metadata structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,20 +28,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta property="og:title" content="Chaimaa Safi" />
-        <meta
-          property="og:description"
-          content="Welcome to my portfolio!Enjoy your visit"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="" />
-        <meta property="og:image:secure_url" content="" />
-        <meta
-          property="og:url"
-          content="https://portfolio-v2-phi-bay.vercel.app/assets/og-image.png"
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
