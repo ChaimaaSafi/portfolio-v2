3ad33cc30c9dec2546c283e2a8ab1a50416d2ac5
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-30 22:14:54 +0100

    feat: add aria label to links

--- src/app/components/core/ShareTo/index.tsx
+++ src/app/components/core/ShareTo/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import FacebookIcon from "@/icons/SocialLinks/FacebookIcon";
 import GithubIcon from "@/icons/SocialLinks/GithubIcon";
 import LinkedinIcon from "@/icons/SocialLinks/LinkedinIcon";
-import RedditIcon from "@/icons/SocialLinks/RedditIcon";
 import TwitterIcon from "@/icons/SocialLinks/TwitterIcon";
 
 export const TwitterShare = ({
@@ -15,7 +14,12 @@ export const TwitterShare = ({
 }) => {
   const ShareURL = `https://twitter.com/intent/tweet?text=${title}&url=https://www.obytes.com/blog/${slug}`;
   return (
-    <a href={ShareURL} target="_blank" rel="noopener noreferrer">
+    <a
+      href={ShareURL}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="twitter"
+    >
       <TwitterIcon className="fill-[#7D7D7D] hover:fill-[#1DA1F2]" />
     </a>
   );
@@ -31,7 +35,12 @@ export const FacebookShare = ({
   const ShareURL = `https://www.facebook.com/sharer/sharer.php?u=https://www.obytes.com/blog/${slug}&quote=${title}`;
 
   return (
-    <a href={ShareURL} target="_blank" rel="noopener noreferrer">
+    <a
+      href={ShareURL}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="facebook"
+    >
       <FacebookIcon className="fill-[#7D7D7D] hover:fill-[#4267B2]" />
     </a>
   );
@@ -41,24 +50,13 @@ export const LinkedinShare = ({ slug }: { slug: string }) => {
   const ShareURL = `https://www.linkedin.com/sharing/share-offsite/?url=https://www.obytes.com/blog/${slug}`;
 
   return (
-    <a href={ShareURL} target="_blank" rel="noopener noreferrer">
+    <a
+      href={ShareURL}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="linnkedin"
+    >
       <LinkedinIcon className="fill-[#7D7D7D] hover:fill-[#0A66C2]" />
-    </a>
-  );
-};
-
-export const RedditShare = ({
-  slug,
-  title,
-}: {
-  slug: string;
-  title: string;
-}) => {
-  const ShareURL = `http://www.reddit.com/submit?url=https://www.obytes.com/blog/${slug}&title=${title}`;
-
-  return (
-    <a href={ShareURL} target="_blank" rel="noopener noreferrer">
-      <RedditIcon className="fill-[#7D7D7D] hover:fill-[#FF5700]" />
     </a>
   );
 };
@@ -67,7 +65,12 @@ export const RedditShare = ({
 
 export const Linkedin = ({ url }: { url: string }) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="linkedin"
+    >
       <LinkedinIcon className="fill-[#fff] hover:shadow-md hover:shadow-gray-200 rounded-md transition duration-300 delay-100" />
     </a>
   );
@@ -75,7 +78,12 @@ export const Linkedin = ({ url }: { url: string }) => {
 
 export const Twitter = ({ url }: { url: string }) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="twitter"
+    >
       <TwitterIcon className="fill-[#fff] hover:shadow-md hover:shadow-gray-200 rounded-md transition duration-300 delay-100" />
     </a>
   );
@@ -83,8 +91,8 @@ export const Twitter = ({ url }: { url: string }) => {
 
 export const GitHub = ({ url }: { url: string }) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
-      <GithubIcon  className="fill-[#fff] hover:shadow-md hover:shadow-gray-200 rounded-full transition duration-300 delay-100"/>
+    <a href={url} target="_blank" rel="noopener noreferrer" aria-label="github">
+      <GithubIcon className="fill-[#fff] hover:shadow-md hover:shadow-gray-200 rounded-full transition duration-300 delay-100" />
     </a>
   );
 };
