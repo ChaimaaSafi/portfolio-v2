2e28be46333f429e6f2bc1437c761ad24ebcf0f0
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-30 22:41:22 +0100

    fix:build error

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,27 +2,29 @@
 import Image from "next/image";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { allPosts } from "contentlayer/generated";
-import { FacebookShare, LinkedinShare, RedditShare, TwitterShare } from "@/components/core/ShareTo";
-
+import {
+  FacebookShare,
+  LinkedinShare,
+  TwitterShare,
+} from "@/components/core/ShareTo";
 
 export default function Page({ params }: any) {
-
   const post = allPosts.find((p) => p.slug === params.slug);
   //@ts-ignore
   const MDXContent = useMDXComponent(post?.body?.code);
 
-  if(!post){
+  if (!post) {
     return (
       <div>
         <p>Something Wrong</p>
       </div>
-    )
+    );
   }
 
   return (
     <section className="w-full ">
-      <div  className="relative mx-auto flex max-w-[1250px] flex-col pt-10  lg:py-5">
-      <h1 className="mx-5 py-5 text-center text-2xl font-medium  lg:m-6 lg:p-10 lg:text-4xl">
+      <div className="relative mx-auto flex max-w-[1250px] flex-col pt-10  lg:py-5">
+        <h1 className="mx-5 py-5 text-center text-2xl font-medium  lg:m-6 lg:p-10 lg:text-4xl">
           {post?.title}
         </h1>
         <div className="mx-2 lg:h-auto lg:flex-1">
@@ -50,14 +52,11 @@ export default function Page({ params }: any) {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center space-y-5 lg:mt-10 my-10">
-        <span className="text-lg leading-6 text-gray-900">
-          Share article
-        </span>
+        <span className="text-lg leading-6 text-gray-900">Share article</span>
         <div className="mt-5 flex space-x-5">
           <TwitterShare slug={post?.slug} title={post.title} />
           <FacebookShare slug={post?.slug} title={post.title} />
           <LinkedinShare slug={post?.slug} />
-          <RedditShare slug={post?.slug} title={post.title} />
         </div>
       </div>
     </section>
