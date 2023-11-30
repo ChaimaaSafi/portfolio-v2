8c5db7b0d0b73fd9a5bec404a1e2918cd70de85e
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-30 22:41:14 +0100

    fix: hover button bg color

--- src/app/components/core/Button.tsx
+++ src/app/components/core/Button.tsx
@@ -2,7 +2,8 @@ import React, { ReactNode } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 const VARIANTS: { [key: string]: string } = {
-  primary: "bg-yellow-primary text-black-hero px-5 py-3 ",
+  primary:
+    "bg-gradient-to-bl from-orange-200  text-gray-800 via-yellow-400 to-rose-400 px-5 py-3 ",
   secondary: "bg-blue-5 px-8 py-2 text-blue-1",
   border:
     "border-2 border-blue-1 bg-transparent px-5 py-1.5 text-blue-1 hover:bg-[#F4F9FD] duration-200",
@@ -26,7 +27,7 @@ function Button({ type, icon, className, children }: TButton) {
           filter: "brightness(1.2)",
         }}
         whileTap={{ scale: 0.9 }}
-        className={`${className} flex ml-1 text-white cursor-pointer items-center justify-center space-x-2  rounded-md text-sm lg:text-lg font-semibold normal-case 
+        className={`${className} flex ml-1   cursor-pointer items-center justify-center space-x-2  rounded-md text-sm lg:text-lg font-semibold normal-case 
          disabled:cursor-not-allowed disabled:opacity-50 shadow shadow-gray-50 ${VARIANTS[type]}`}
       >
         {icon && <p>{icon}</p>}
